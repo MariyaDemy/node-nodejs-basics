e5cc92812badb9b4e2717d80dc60b97cd02ae1d8
Mariya Demy <Mariya.Demi373@gmail.com>
2023-01-19 16:55:23 +0300

    feat: implement fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile } from 'node:fs/promises';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(
+        filePath,
+        'I am fresh and young',
+        {flag: 'wx'} // if file already exists, there will be an error
+      );
+  } catch(error) {
+        throw Error('FS operation failed');
+  }
 };
 
 await create();
