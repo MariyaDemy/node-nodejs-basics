d982a20ac436a813965f1c5456ecc037925b4d6e
Mariya Demy <Mariya.Demi373@gmail.com>
2023-01-22 13:47:07 +0300

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { join } from "path";
+import { getDirName } from "../../fileInfo.js";
+import { readFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
+
+const __dirname = getDirName(import.meta.url);
+let filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileContents = await readFile(filePath);
+    const hexHash = createHash("sha256").update(fileContents).digest("hex");
+    console.log(hexHash);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await calculateHash();
