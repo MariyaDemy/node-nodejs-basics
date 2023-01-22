4be5877a269317a5a38f4f6d9368f047ce3293af
Mariya Demy <Mariya.Demi373@gmail.com>
2023-01-23 00:37:28 +0300

    feat: implement cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argvArr = process.argv.slice(2);
+  const argsArr = argvArr.reduce((arr, item, index, array) => {
+    if (index % 2 === 0) {
+      arr.push(`${item.replace(/--/, "")} is ${array[index + 1]}`);
+    }
+    return arr;
+  }, []);
+  const resultString = argsArr.join(", ");
+  console.log(resultString);
 };
 
 parseArgs();
