be6a25f436c83660876da71a13753e0eface4b46
Mariya Demy <Mariya.Demi373@gmail.com>
2023-01-23 15:54:28 +0300

    feat: implement streams/write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,4 +1,4 @@
-import { createReadStream } from 'node:fs';
+import { createReadStream } from "node:fs";
 import { join } from "path";
 import { getDirName } from "../../fileInfo.js";
 
@@ -6,9 +6,9 @@ const __dirname = getDirName(import.meta.url);
 const readFile = join(__dirname, "files", "fileToRead.txt");
 
 const read = async () => {
-    const readableStream = createReadStream(readFile, "utf8");
-    const writeableStream = process.stdout;
-    readableStream.pipe(writeableStream);
+  const readableStream = createReadStream(readFile, "utf8");
+  const writeableStream = process.stdout;
+  readableStream.pipe(writeableStream);
 };
 
 await read();
