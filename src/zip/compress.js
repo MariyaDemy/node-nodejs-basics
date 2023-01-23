3393844ee0253bbc6f25152669bb5b3d2d2822e9
Mariya Demy <Mariya.Demi373@gmail.com>
2023-01-23 17:56:24 +0300

    feat:implement zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { join } from "path";
+import { getDirName } from "../../fileInfo.js";
+import { pipeline } from "node:stream/promises";
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+
+const __dirname = getDirName(import.meta.url);
+const initialFile = join(__dirname, "files", "fileToCompress.txt");
+const compressedFile = join(__dirname, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(initialFile, "utf-8");
+  const writeableStream = createWriteStream(compressedFile);
+
+  await pipeline(readableStream, createGzip(), writeableStream);
 };
 
 await compress();
