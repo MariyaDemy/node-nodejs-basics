00dbe04819db8168e0019d6ae8dc869a35deb6b7
Mariya Demy <Mariya.Demi373@gmail.com>
2023-01-23 16:36:00 +0300

    feat: implement streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const readableStream = process.stdin;
+  const writeableStream = process.stdout;
+
+  const reverseText = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(
+        null,
+        chunk.toString().trim().split("").reverse().join("") + "\n"
+      );
+    },
+  });
+
+  await pipeline(readableStream, reverseText, writeableStream);
 };
 
 await transform();
