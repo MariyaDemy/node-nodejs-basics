128fc156475e0b6fc2b55b4ca5052b07b7d5f5de
Mariya Demy <Mariya.Demi373@gmail.com>
2023-01-26 00:50:24 +0300

    feat: implement wt/main, wt/worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { cpus } from "node:os";
+import { Worker } from "node:worker_threads";
+import { getDirName } from "../../fileInfo.js";
+import { join } from "path";
+
+const __dirname = getDirName(import.meta.url);
+const workerPath = join(__dirname, "./worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+
+  const logicalCPUCoresNumber = cpus().length;
+
+  function createWorker(workerData) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, { workerData: workerData });
+      worker.on("message", (message) => {
+        resolve({ status: "resolved", data: message });
+      });
+      // "resolve" to pass it to the promise.all even if there is an error
+      worker.on("error", (error) => {
+        resolve({ status: "error", data: null });
+      });
+    });
+  }
+
+  const promisesArr = [];
+
+  let i = 10;
+  let n = 0;
+
+  while (n < logicalCPUCoresNumber) {
+    const workerPromise = createWorker(i);
+    promisesArr.push(workerPromise);
+    i++;
+    n++;
+  }
+
+  Promise.all(promisesArr)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
 await performCalculations();
